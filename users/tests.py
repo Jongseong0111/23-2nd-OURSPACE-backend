8c9ffd9705cc88880bff55e71851dd1eab0b3f67
Jang184 <e2joo418@gmail.com>
2021-08-19 16:37:55 +0900

    카카오 소셜로그인 기능 구현

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,76 @@
-from django.test import TestCase
+from users.models  import User
+from django.test   import TestCase, Client
+from unittest.mock import patch, MagicMock
 
-# Create your tests here.
+from my_settings import SECRET_KEY
+
+class KaKaoSignInTest(TestCase):
+    def setUp(self):
+        User.objects.create(
+            kakao_id = '140327275127424',
+            nickname = 'jang',
+            email = 'luv4cece@naver.com'
+        )   
+
+    def tearDown(self):
+        User.objects.all().delete()
+
+    @patch('users.views.requests')    
+    def test_kakaologin_view_get_signup_success(self, mocked_requests):
+        client = Client()
+
+        class MockedResponse:
+            def json(self):
+                return {
+                    'id': '1408384944',
+                    'kakao_account': {
+                    'email' : 'juri@naver.com'
+                    },
+                    'properties' : {
+                        'nickname' : 'juri'
+                    }
+                }
+        mocked_requests.get = MagicMock(return_value = MockedResponse())
+        header              = {'HTTP_Authorization' : 'access_token'}
+        response            = client.get('/users/signin/kakao', ContentType ='application/json',**header)
+        self.assertEqual(response.status_code, 200)
+
+    @patch('users.views.requests')
+    def test_kakaologin_view_get_signin_success(self, mocked_requests):
+        client = Client()
+
+        class MockedResponse:
+            def json(self):
+                return {
+                    'id': '140327275127424',
+                    'kakao_account': {
+                    'email' : 'luv4cece@naver.com'
+                    },
+                    'properties' : {
+                        'nickname' : 'jang'
+                    }
+                }
+        mocked_requests.get = MagicMock(return_value = MockedResponse())
+        header              = {'HTTP_Authorization' : 'access_token'}
+        response            = client.get('/users/signin/kakao', ContentType ='application/json',**header)
+        self.assertEqual(response.status_code, 200)
+
+    @patch('users.views.requests')
+    def test_kakaologin_view_get_no_token(self, request):
+        client   = Client()
+        response = client.get('/users/signin/kakao')
+        self.assertEqual(response.json(),{'message':'no_token'})
+        self.assertEqual(response.status_code, 401)
+
+    @patch('users.views.requests')
+    def test_kakaologin_view_get_invalid_token(self, request):
+        client = Client()
+
+        class MockedResponse:
+            def json(self):
+                return {}
+        request.get = MagicMock(return_value = MockedResponse())
+        header      = {'HTTP_Authorization' : 'access_token'}
+        response    = client.get('/users/signin/kakao', ContentType ='application/json',**header)
+        self.assertEqual(response.json(), {'message':'invalid_token'})
+        self.assertEqual(response.status_code, 400)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+import json, jwt, requests, random
 
-# Create your views here.
+from django.http  import JsonResponse
+from django.views import View
+
+from users.models import User
+from my_settings  import SECRET_KEY
+
+class KakaoSignInView(View): 
+    def get(self, request): 
+        try:
+            access_token = request.headers.get('Authorization')
+
+            if not access_token: 
+                return JsonResponse({'message':'no_token'}, status=401)
+
+            url           = 'https://kapi.kakao.com/v2/user/me'
+            response      = requests.get(url, headers={'Authorization': f'Bearer {access_token}'})
+            user_response = response.json()
+        
+            user,created = User.objects.get_or_create(kakao_id=user_response['id'])
+
+            if created: 
+                user.email    = user_response['kakao_account']['email']
+                user.nickname = user_response['properties']['nickname']
+                user.point    = random.randrange(0,10000)
+                user.save()
+
+            token = jwt.encode({'id':user.id}, SECRET_KEY, algorithm= 'HS256')
+            return JsonResponse({'TOKEN':token}, status=200)
+        except KeyError:
+            return JsonResponse({'message':'invalid_token'}, status=400)
+        
