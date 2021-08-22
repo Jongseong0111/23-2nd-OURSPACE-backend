34762734c163b885d40549b7eb4a812fe7f74751
Jang184 <e2joo418@gmail.com>
2021-08-22 16:17:43 +0900

    공간등록 기능 구현

--- spaces/urls.py
+++ spaces/urls.py
@@ -1,8 +1,11 @@
 from django.urls   import path
-from spaces.views  import ProductsView, DistrictView, CategoryView
+
+from spaces.views  import FacilityView, ProductsView, DistrictView, HostView, CategoryView
 
 urlpatterns = [
     path('', ProductsView.as_view()),
     path('/district', DistrictView.as_view()),
     path('/category', CategoryView.as_view()),
+    path('/host', HostView.as_view()),
+    path('/facility', FacilityView.as_view())
 ]
