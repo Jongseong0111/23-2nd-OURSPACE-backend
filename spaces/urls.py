c6a3e2893431c4ebadd3c616e5136b9f0d7637b9
임종성 <qkqndudnxld@gmail.com>
2021-08-24 10:33:24 +0900

    Category2

--- spaces/urls.py
+++ spaces/urls.py
@@ -1,7 +1,8 @@
 from django.urls   import path
-from spaces.views  import ProductsView, DistrictView
+from spaces.views  import ProductsView, DistrictView, CategoryView
 
 urlpatterns = [
     path('', ProductsView.as_view()),
     path('/district', DistrictView.as_view()),
+    path('/category', CategoryView.as_view()),
 ]
